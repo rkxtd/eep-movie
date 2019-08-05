fc2e5f907bafed574c3b1c17751a16e0a7de5163
Roman Shuvaryk <xcomanche@gmail.com>
2019-08-05 16:24:35 -0700

    Added emoji

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import MovieList from '../../components/MovieList';
 import movies from './movies';
+import randomTextMeme from 'random-text-meme';
 
 export default function HomePage() {
     return (
         <div style={{minWidth: 360}}>
-            Home Page
+            Home Page {randomTextMeme.getEmoji('flip-table')}
             <MovieList movies={movies} />
         </div>
     );
